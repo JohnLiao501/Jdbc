8b2eb36824c8620863bec8f9a235918590f973fd
John Liao <johnliao381658675@outlook.com>
2024-06-07 12:19:04 +0800

    注册页面开发

--- src/com/cn/shixun/pojo/Result.java
+++ src/com/cn/shixun/pojo/Result.java
@@ -3,15 +3,15 @@ package com.cn.shixun.pojo;
 public class Result {
 
     private int code;
-
     private String info;
-
     private Object data;
 
     public Result(int code, String info, Object data) {
         this.code = code;
         this.info = info;
         this.data = data;
+    }
+    public Result() {
     }
     public int getCode() {
         return code;
@@ -22,5 +22,14 @@ public class Result {
     public String getInfo() {
         return info;
     }
-
+    public void setInfo(String info) {
+        this.info = info;
+    }
+    public Object getData() {
+        return data;
+    }
+    @Override
+    public String toString() {
+        return "Result [code=" + code + ", info=" + info + ", data=" + data + "]";
+    }
 }
--- src/com/cn/shixun/pojo/User.java
+++ src/com/cn/shixun/pojo/User.java
@@ -2,4 +2,23 @@ package com.cn.shixun.pojo;
 
 public class User {
     private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+    public User() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
